56038416f9747d5730570b4a9280b6efd46729ec
survivecz <poucpetr11@seznam.cz>
2023-12-07 14:16:51 +0100

    feature engineering

--- feature_engineering_cli/feature_engineering_cli/cli.py
+++ feature_engineering_cli/feature_engineering_cli/cli.py
@@ -7,92 +7,109 @@ import matplotlib.pyplot as plt
 import logging
 from pyarrow import Table
 from colorama import init, Fore, Style
+import pickle
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning, module="pandas.api.types")
+warnings.filterwarnings("ignore", message="is_sparse is deprecated", category=FutureWarning)
+warnings.filterwarnings("ignore", message="is_categorical_dtype is deprecated", category=FutureWarning)
+
+class FeatureEngineeringCLI:
+    def __init__(self, benign_path: str, malign_path: str):
+        self.benign_path = benign_path
+        self.malign_path = malign_path
+        self.logger = self.configure_logger()
+        self.DEFAULT_INPUT_DIR = "../../floor"
+        self.nontraining_fields = [
+            "dns_evaluated_on",
+            "rdap_evaluated_on",
+            "tls_evaluated_on",
+            "ip_data",
+            "countries",
+            "latitudes",
+            "longitudes",
+            "dns_dnssec",
+            "dns_zone_dnskey_selfsign_ok",
+            "dns_email_extras",
+            "dns_ttls",
+            "dns_zone",
+            "dns_zone_SOA",
+            *[f"dns_{t}" for t in ('A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'TXT')],
+            "rdap_registration_date",
+            "rdap_last_changed_date",
+            "rdap_expiration_date",
+            "rdap_dnssec",
+            "rdap_entities"
+        ]
+    def print_header(self, message: str) -> None:
+        header = f"{'=' * len(message)}"
+        self.logger.info(self.color_log(header, Fore.CYAN))
+        self.logger.info(self.color_log(message, Fore.CYAN))
+        self.logger.info(self.color_log(header, Fore.CYAN))
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+    def configure_logger(self) -> logging.Logger:
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
 
-# Create a file handler and set the logging level
-file_handler = logging.FileHandler('feature_engineering.log')
-file_handler.setLevel(logging.INFO)
+        file_handler = logging.FileHandler('feature_engineering.log')
+        file_handler.setLevel(logging.INFO)
 
-# Create a console handler and set the logging level
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
 
-# Create a log format
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
 
-# Add the file and console handlers to the logger
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
-def color_log(message, color=Fore.GREEN):
-    return f"{color}{message}{Style.RESET_ALL}"
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
 
+        return logger
 
-#print szs path
-DEFAULT_INPUT_DIR = "../../floor"
-nontraining_fields = [
-    #"domain_name",
-    "dns_evaluated_on",
-    "rdap_evaluated_on",
-    "tls_evaluated_on",
+    def color_log(self, message: str, color: str = Fore.GREEN) -> str:
+        return f"{color}{message}{Style.RESET_ALL}"
 
-    # IP data
-    "ip_data",
-    "countries",
-    "latitudes",
-    "longitudes",
+    def load_pickled_data(self) -> tuple:
+        shap_values = pickle.load(open("../../shap_values.pickle.dat", "rb"))
+        model = pickle.load(open("../../xgboost_model.pickle.dat", "rb"))
+        X_train = pickle.load(open("../../X_train.pickle.dat", "rb"))
+        return shap_values, model, X_train
 
-    # DNS
-    "dns_dnssec",
-    "dns_zone_dnskey_selfsign_ok",
-    "dns_email_extras",
-    "dns_ttls",
-    "dns_zone",
-    "dns_zone_SOA",
-    *[f"dns_{t}" for t in ('A', 'AAAA', 'CNAME', 'MX', 'NS', 'SOA', 'TXT')],
+    def drop_nontrain(self, table: Table) -> Table:
+        """
+        Drop non-training columns.
+        """
+        fields = [x for x in self.nontraining_fields if x in table.column_names]
+        return table.drop(fields)
 
-    "rdap_registration_date",
-    "rdap_last_changed_date",
-    "rdap_expiration_date",
-    "rdap_dnssec",
-    "rdap_entities"
+    def get_feature_with_highest_shap(self, shap_values: np.ndarray, dataset: pd.DataFrame, sample_index: int) -> tuple:
+        abs_shap_values = np.abs(shap_values[sample_index, :])
+        highest_shap_index = np.argmax(abs_shap_values)
 
-    #"tls_root_cert_validity_remaining",
-    #"tls_leaf_cert_validity_remaining"
-]
+        # Get the corresponding feature name and value from the dataset
+        feature_name = dataset.columns[highest_shap_index]
+        feature_value = dataset.iloc[sample_index, highest_shap_index]
 
-def drop_nontrain(table: Table):
-    """
-    Drop non-training columns.
-    """
-    fields = [x for x in nontraining_fields if x in table.column_names]
-    return table.drop(fields)
+        return feature_name, feature_value
 
-@click.command()
-@click.option('--benign', '-b', help='Filename of benign dataset')
-@click.option('--malign', '-m', help='Filename of malign dataset')
-@click.option('--time-analysis', '-ta', is_flag=True, help='Perform time analysis')
-def feature_engineering(benign, malign, time_analysis):
-    benign_path = os.path.join(DEFAULT_INPUT_DIR, benign) if benign else None
-    malign_path = os.path.join(DEFAULT_INPUT_DIR, malign) if malign else None
-
-    if benign_path and malign_path:
-        logger.info(color_log(f'Performing feature engineering on benign dataset: {benign_path} and malign dataset: {malign_path}', Fore.BLUE))
-        # Read benign and malign datasets
+    def perform_feature_engineering(self) -> None:
+        benign_path = os.path.join(self.DEFAULT_INPUT_DIR, self.benign_path) if self.benign_path else None
+        malign_path = os.path.join(self.DEFAULT_INPUT_DIR, self.malign_path) if self.malign_path else None
+        #load shap values, model and X_train
+        shap_values, model, X_train = self.load_pickled_data()
+        
+        self.print_header("Performing Feature Engineering")
+        self.logger.info(f'Benign dataset path: {benign_path}')
+        self.logger.info(f'Malign dataset path: {malign_path}')       
         data = pq.read_table(benign_path)
         data2 = pq.read_table(malign_path)
 
-    if time_analysis:
-        #log that we are determining which features from tls and rdap are time dependant
-        logger.info(color_log('Determining which features from tls and rdap are time dependent', Fore.YELLOW))
-        data = drop_nontrain(data)
-        data2 = drop_nontrain(data2)
+        # Drop non-training columns
+        data = self.drop_nontrain(data)
+        data2 = self.drop_nontrain(data2)
 
-        # Convert to pandas DataFrame
+        # Convert to pandas DataFrame if needed
         df1 = data.to_pandas()
         df2 = data2.to_pandas()
 
@@ -111,8 +128,6 @@ def feature_engineering(benign, malign, time_analysis):
 
         ratios = {}
         for feature in selected_features_without_age:
-            print(f"Feature: {feature}, Data Type: {df1[feature].dtype}")
-
             benign_count = df1[feature].count()
             phishing_count = df2[feature].count()
             
@@ -133,12 +148,11 @@ def feature_engineering(benign, malign, time_analysis):
             if phishing_count != 0:
                 ratio = benign_count / phishing_count
                 ratios[col] = ratio
-
+        for key in ratios:
+            self.logger.info(self.color_log(f'Possibly time demanding feature: {key}', Fore.YELLOW))
         # Sort features by ratio in descending order (including the timedelta columns)
         sorted_features = sorted(ratios, key=ratios.get, reverse=True)[:9]  # Select top 9 features
         custom_colors = ['#1f77b4', '#ff7f0e']  # Blue for Benign, Orange for Phishing
-
-        # Plot histograms for the top 9 features in a 3x3 subplot
         fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15, 15))
 
         for i, feature in enumerate(sorted_features):
@@ -166,11 +180,41 @@ def feature_engineering(benign, malign, time_analysis):
 
         # Set overall figure title
         fig.suptitle('Relative Frequency Histograms for Top Features', fontsize=16)
-
         plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Adjust subplot layout and spacing
         plt.savefig('../../false_positives/images/relative_freq_histograms.png', dpi=300)  # Save the plot as a high-resolution PNG
+        plt.close()
+
         
-        logger.info(color_log('Saved possible time dependand features of TLS and RDAP to ../../false_positives/images/relative_freq_histograms.png', Fore.GREEN))
+        self.logger.info(self.color_log('Choosing most relevant features based on feature_importances_', Fore.YELLOW))
+        feature_importances = model.feature_importances_
+        normalized_importances = (feature_importances - np.min(feature_importances)) / (np.max(feature_importances) - np.min(feature_importances))
+        # Get the indices that would sort the importances array
+        sorted_indices = np.argsort(feature_importances)[::-1]
+        top_20_indices = sorted_indices[:20]
+
+        # Plotting the top 20 feature importances
+        plt.figure(figsize=(10, 8))
+        sns.barplot(x=normalized_importances[top_20_indices], y=X_train.columns[top_20_indices], orient='h')
+        plt.title('Top 20 Feature Importances')
+        plt.xlabel('Importance')
+        plt.ylabel('Features')
+        plt.tight_layout(pad=1.0)  # Increase padding to avoid cropping
+        plt.savefig('../../false_positives/images/top_20_feature_importances.png', dpi=300)  # Save the plot as a high-resolution PNG
+
+        self.print_header("Saving Results")
+        
+        self.logger.info(self.color_log('Saved possible time-dependent features to:'
+                         ' ../../false_positives/images/relative_freq_histograms.png', Fore.GREEN))
+        self.logger.info(self.color_log('Saved top 20 feature importances plot to:'
+                         ' ../../false_positives/images/top_20_feature_importances.png', Fore.GREEN))
+
+
+@click.command()
+@click.option('--benign', '-b', help='Filename of benign dataset')
+@click.option('--malign', '-m', help='Filename of malign dataset')
+def feature_engineering(benign: str, malign: str) -> None:
+    fe_cli = FeatureEngineeringCLI(benign, malign)
+    fe_cli.perform_feature_engineering()
 
 if __name__ == '__main__':
     feature_engineering()
